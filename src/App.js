a94d72c833e3df8877a4ca89c05fde5207b548d4
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-06-25 21:00:57 -0500

    Feat: setting up state; implement removing contact using setState

--- src/App.js
+++ src/App.js
@@ -23,10 +23,39 @@ const contacts = [
 ]
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+          'id': 'ryan',
+          'name': 'Ryan Florence',
+          'email': 'ryan@reacttraining.com',
+          'avatarURL': 'http://localhost:5001/ryan.jpg'
+      },
+      {
+          'id': 'michael',
+          'name': 'Michael Jackson',
+          'email': 'michael@reacttraining.com',
+          'avatarURL': 'http://localhost:5001/michael.jpg'
+      },
+      {
+          'id': 'tyler',
+          'name': 'Tyler McGinnis',
+          'email': 'tyler@reacttraining.com',
+          'avatarURL': 'http://localhost:5001/tyler.jpg'
+      }
+    ]
+  }
+
+  removeContact = contact => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(c => c.id!==contact.id)
+    }))
+  }
+
   render() {
     return (
       <div className="App">
-        <ListContacts contacts={contacts} />
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
       </div>
     );
   }
